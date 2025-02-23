0f21581b267e656de8f3fe48da4ca1295b321b64
DimaBohdan <bohdan777.dmytro@gmail.com>
2025-02-23 17:30:28 +0200

    db-feat: create PrismaORM schema and migrations

---
+++ questplatform-api/prisma/migrations/20250223093227_init/migration.sql
@@ -0,0 +1,223 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "QuestTaskType" AS ENUM ('SINGLE_CHOICE', 'MULTIPLE_CHOICE', 'TEXT_FIELD', 'FIND_ON_PICTURE', 'FIND_ON_MAP', 'PLOT');
+
+-- CreateEnum
+CREATE TYPE "MediaFileType" AS ENUM ('IMAGE', 'VIDEO');
+
+-- CreateEnum
+CREATE TYPE "Category" AS ENUM ('GAMES', 'SPORT', 'ANIME', 'MANGA', 'FILMS', 'STUDY', 'PSYCHOLOGY', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('PENDING', 'APPROVED', 'DECLINED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "nickname" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "avatar" TEXT,
+    "info" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "completedQuestIds" TEXT[],
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "friendships" (
+    "requesterId" TEXT NOT NULL,
+    "receiverId" TEXT NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "friendships_pkey" PRIMARY KEY ("requesterId","receiverId")
+);
+
+-- CreateTable
+CREATE TABLE "Quest" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "authorId" TEXT NOT NULL,
+    "time" DOUBLE PRECISION,
+    "difficulty" INTEGER NOT NULL,
+    "category" "Category" NOT NULL,
+    "tags" TEXT[],
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
+    "rating" DOUBLE PRECISION,
+    "taskQuantity" INTEGER NOT NULL DEFAULT 0,
+    "themeId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Quest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserCompletedQuest" (
+    "userId" TEXT NOT NULL,
+    "questId" TEXT NOT NULL,
+
+    CONSTRAINT "UserCompletedQuest_pkey" PRIMARY KEY ("userId","questId")
+);
+
+-- CreateTable
+CREATE TABLE "quest_tasks" (
+    "id" TEXT NOT NULL,
+    "questId" TEXT NOT NULL,
+    "question" TEXT NOT NULL,
+    "type" "QuestTaskType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "answerIds" TEXT[],
+    "textAnswer" TEXT,
+    "targetX" DOUBLE PRECISION,
+    "targetY" DOUBLE PRECISION,
+    "radius" DOUBLE PRECISION,
+    "content" TEXT,
+    "choices" JSONB,
+
+    CONSTRAINT "quest_tasks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "media_files" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "type" "MediaFileType" NOT NULL,
+    "width" INTEGER,
+    "height" INTEGER,
+    "duration" DOUBLE PRECISION,
+    "taskId" TEXT,
+    "optionId" TEXT,
+    "questId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "media_files_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Option" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "mediaId" TEXT,
+    "taskId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isCorrect" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Option_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "QuestReview" (
+    "id" TEXT NOT NULL,
+    "score" INTEGER NOT NULL,
+    "comment" TEXT,
+    "authorId" TEXT NOT NULL,
+    "questId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "QuestReview_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "QuestTheme" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "background" TEXT,
+    "textColor" TEXT,
+    "fontStyle" TEXT,
+    "script" TEXT,
+
+    CONSTRAINT "QuestTheme_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Achievement" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "condition" JSONB NOT NULL,
+
+    CONSTRAINT "Achievement_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_AchievementToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_AchievementToUser_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_nickname_key" ON "User"("nickname");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "media_files_url_key" ON "media_files"("url");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "media_files_taskId_key" ON "media_files"("taskId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "media_files_optionId_key" ON "media_files"("optionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "media_files_questId_key" ON "media_files"("questId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "QuestTheme_name_key" ON "QuestTheme"("name");
+
+-- CreateIndex
+CREATE INDEX "_AchievementToUser_B_index" ON "_AchievementToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "friendships" ADD CONSTRAINT "friendships_requesterId_fkey" FOREIGN KEY ("requesterId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "friendships" ADD CONSTRAINT "friendships_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Quest" ADD CONSTRAINT "Quest_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Quest" ADD CONSTRAINT "Quest_themeId_fkey" FOREIGN KEY ("themeId") REFERENCES "QuestTheme"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserCompletedQuest" ADD CONSTRAINT "UserCompletedQuest_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserCompletedQuest" ADD CONSTRAINT "UserCompletedQuest_questId_fkey" FOREIGN KEY ("questId") REFERENCES "Quest"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "quest_tasks" ADD CONSTRAINT "quest_tasks_questId_fkey" FOREIGN KEY ("questId") REFERENCES "Quest"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "media_files" ADD CONSTRAINT "media_files_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "quest_tasks"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "media_files" ADD CONSTRAINT "media_files_optionId_fkey" FOREIGN KEY ("optionId") REFERENCES "Option"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "media_files" ADD CONSTRAINT "media_files_questId_fkey" FOREIGN KEY ("questId") REFERENCES "Quest"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Option" ADD CONSTRAINT "Option_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "quest_tasks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "QuestReview" ADD CONSTRAINT "QuestReview_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "QuestReview" ADD CONSTRAINT "QuestReview_questId_fkey" FOREIGN KEY ("questId") REFERENCES "Quest"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_AchievementToUser" ADD CONSTRAINT "_AchievementToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Achievement"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_AchievementToUser" ADD CONSTRAINT "_AchievementToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
