4feb8c9238adb78c09812ef112820f7ee559560c
DimaBohdan <bohdan777.dmytro@gmail.com>
2025-04-09 15:59:36 +0300

    feat(UserQuestProgress): implement module for joining to running session for each user

--- questplatform-api/src/database/user-quest-progress.repository.ts
+++ questplatform-api/src/database/user-quest-progress.repository.ts
@@ -1,57 +1,29 @@
 import { Injectable } from "@nestjs/common";
-import { UserQuestProgress } from "@prisma/client";
+import { QuestRunMode, UserQuestProgress } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class UserQuestProgressRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async findLastProgress(userId: string, questId: string): Promise<UserQuestProgress | null> {
-    return await this.prisma.userQuestProgress.findFirst({
-      where: { userId, questId, completed: false },
-      include: { currentTask: true },
-    });
-  }
-  
-  async create(userId: string, questId: string, currentTaskId: string): Promise<UserQuestProgress> {
+  async createProgress(userId: string, runId: string): Promise<UserQuestProgress> {
     return await this.prisma.userQuestProgress.create({
       data: {
         userId,
-        questId,
-        currentTaskId,
-        startTime: new Date(),
-      },
-      include: { currentTask: true },
+        runId,
+      }
     });
   }
 
-  async progressNewTask(userId: string, questId: string, currentTaskId: string): Promise<UserQuestProgress[]> {
-    await this.prisma.userQuestProgress.updateMany({
-      where: { userId, questId },
-      data: { currentTaskId: currentTaskId },
+  async findProgress(userId: string, runId: string): Promise<UserQuestProgress | null> {
+    return await this.prisma.userQuestProgress.findFirst({
+      where: { userId, runId },
     });
+  }
 
+  async findProgressByRun(runId: string): Promise<UserQuestProgress[]> {
     return await this.prisma.userQuestProgress.findMany({
-      where: { userId, questId },
+      where: { runId },
     });
   }
-
-  async completeProgress(userId: string, questId: string): Promise<UserQuestProgress[]> {
-    const result = await this.prisma.$transaction([
-      this.prisma.userQuestProgress.updateMany({
-        where: { userId, questId },
-        data: { completed: true },
-      }),
-  
-      this.prisma.quest.update({
-        where: { id: questId },
-        data: { playCount: { increment: 1 } },
-      }),
-    ]);
-  
-    return this.prisma.userQuestProgress.findMany({
-      where: { userId, questId },
-    });
-  }
-  
 }
--- questplatform-api/src/user-quest-progress/user-quest-progress.controller.ts
+++ questplatform-api/src/user-quest-progress/user-quest-progress.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post, Req } from '@nestjs/common';
+import { UserQuestProgressService } from './user-quest-progress.service';
+import { RequestWithUser } from 'utils/types/RequestWithUser';
 
 @Controller('user-quest-progress')
-export class UserQuestProgressController {}
+export class UserQuestProgressController {
+  constructor(private readonly userQuestProgressService: UserQuestProgressService) {}
+
+  @Post('join/multiplayer/:code')
+  joinMultiplayer(@Param('code') code: string, @Req() req: RequestWithUser) {
+    const userId = req.user.id;
+    return this.userQuestProgressService.joinMultiplayer(code, userId);
+  }
+}
+
